148762034272f2dc8e0234cb6e0b6aeba04196b0
Vincent Coetzee <vincent@Pink.local>
2020-02-25 20:01:21 +0200

    Initial Commit

---
+++ CobaltX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CobaltX
+//
+//  Created by Vincent Coetzee on 2020/02/25.
+//  Copyright © 2020 Vincent Coetzee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CobaltX/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CobaltX
+//
+//  Created by Vincent Coetzee on 2020/02/25.
+//  Copyright © 2020 Vincent Coetzee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
